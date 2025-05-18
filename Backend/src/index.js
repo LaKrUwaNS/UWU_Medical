541e0dac4b7689297d47d6a97de32973fad5c04a
Your GitHub Username <your@email.com>
2025-05-18 21:01:00 +0530

    create the front end and backend file

---
+++ Backend/src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express(); 
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
+});
+
+
+
+
+app.listen(5000, () => {
+    console.log('Server is running on http://localhost:5000');
+});
