1dce9d2ac322cc829f3c951c1d92383178a457f9
Nirmal2002Cn <chamilanirmal55@gmail.com>
2025-06-24 13:31:57 +0530

    Add SlideBar component with collapsible menu for navigation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -14,6 +14,8 @@ function App() {
     <main className='App'>
 
 
+    <Profile />
+
 
 
 
---
+++ student/src/assets/Image.js
@@ -0,0 +1,27 @@
+import dashboard from './SlideBar/dashboard.svg';
+import logo from './SlideBar/Logo.svg';
+import medicalReq from './SlideBar/medicalReq.svg';
+import medicineData from './SlideBar/medicineData.svg';
+import reminders from './SlideBar/reminders.svg';
+import reports from './SlideBar/reports.svg';
+import settings from './SlideBar/settings.svg';
+import staff from './SlideBar/staff.svg';
+import studentData from './SlideBar/studentData.svg';
+import toggle from './SlideBar/toggle.svg';
+import updates from './SlideBar/updates.svg';
+
+const images = {
+  dashboard,
+  logo,
+  medicalReq,
+  medicineData,
+  reminders,
+  reports,
+  settings,
+  staff,
+  studentData,
+  toggle,
+  updates
+};
+
+export default images;
