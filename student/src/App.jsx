014b7b692b1b0ef507ff9a4bbffe37b6d7fdb606
N.M.L.S.K.Nishshanka <127667099+LaKrUwaNS@users.noreply.github.com>
2025-06-25 17:41:40 +0530

    Update App.jsx

--- student/src/App.jsx
+++ student/src/App.jsx
@@ -1,7 +1,4 @@
 import "./App.css";
-import Loading from "./components/Loading-Animation/Loading";
-import SlideBar from "./components/SlideBar/SlideBar";
-import Home from "./pages/Home/home";
 
 function App() {
   return (
