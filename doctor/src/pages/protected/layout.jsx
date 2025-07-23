a6eb38bcb89b9e8de6971cbde04b170f3f34b46f
Lakruwan Sharaka <your@email.com>
2025-07-24 00:29:45 +0530

    FIX : Make user already loging Error

--- doctor/src/pages/protected/layout.jsx
+++ doctor/src/pages/protected/layout.jsx
@@ -1,30 +1,30 @@
 // src/layout/Layout.jsx
-import React,{useState} from "react";
+import React, { useState } from "react";
 import SlideBar from "../../components/SlideBar/SlideBar";
 import { Navigate } from 'react-router-dom';
 import './layout.css';
 
 
 const Layout = ({ children }) => {
-    const [isCollapsed,setIsCollapsed] = useState(false);
+    const [isCollapsed, setIsCollapsed] = useState(false);
 
 
 
-        const handleToggle = () =>{
-            setIsCollapsed(prev => !prev);
-        };
-    
+    const handleToggle = () => {
+        setIsCollapsed(prev => !prev);
+    };
+
 
     return (
         <div className="main-layout">
-           <div className={`sidebar-container ${isCollapsed ? "collapsed" :""}`}>
+            <div className={`sidebar-container ${isCollapsed ? "collapsed" : ""}`}>
                 <SlideBar isCollapsed={isCollapsed} onToggle={handleToggle} />
 
-           </div>
-           <div className={`main-content ${isCollapsed ? "collapsed" :""}`}>
+            </div>
+            <div className={`main-content ${isCollapsed ? "collapsed" : ""}`}>
                 {children}
 
-           </div>
+            </div>
         </div>
     );
 };
