ff382db11d3e21d200fcb4219de02132a7600caf
kavinduAshen <kavinduashensenarath@gmail.com>
2025-07-30 11:30:15 +0530

    FIX : Add props to Dashboard component

--- doctor/src/components/DonutChart/DonutChart.jsx
+++ doctor/src/components/DonutChart/DonutChart.jsx
@@ -1,39 +1,30 @@
 import React from 'react';
-import {PieChart,Pie,Cell,Legend} from 'recharts';
+import { PieChart, Pie, Cell, Legend } from 'recharts';
 import './DonutChart.css';
 
+const DonutChart = ({ patientData }) => {
+    const COLORS = ['#8884d8', '#00C49F', '#FFBB28', '#FF8042', '#0088FE'];
 
-const data = [
-    { name: 'ICT', value: 5 },
-    { name: 'ET', value: 4 },
-    { name: 'ENM', value: 6 },
-    { name: 'AQT', value: 3 },
-    
-];
-const COLORS = ['#8884d8', '#00C49F', '#FFBB28', '#FF8042'];
-
-const DonutChart = () =>{
-    return(
+    return (
         <div className="donut-chart-container">
-            <h3 className='chart-title'>Patient Departments</h3>
+            <h3 className="chart-title">Patient Departments</h3>
             <PieChart width={400} height={200}>
                 <Pie
-                    data ={data}
-                    cx='50%'
-                    cy='50%'
+                    data={patientData}
+                    cx="50%"
+                    cy="50%"
                     innerRadius={40}
                     outerRadius={70}
                     dataKey="value"
-                    >
-                        {data.map((entry,index) => (
-                            <Cell key={index} fill={COLORS[index % COLORS.length]} />
-                        ))}
-                    </Pie>
-                    <Legend verticalAlign='bottom' height={26} />
+                >
+                    {patientData.map((entry, index) => (
+                        <Cell key={index} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                </Pie>
+                <Legend verticalAlign="bottom" height={26} />
             </PieChart>
         </div>
     );
-
 };
 
 export default DonutChart;
