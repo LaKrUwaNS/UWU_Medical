8b1a19c2b6944c622f80c775daf564bb5e3e6111
LaKrUwaNS <lakruwansharakanishshanka@gmail.com>
2025-08-03 19:50:20 +0530

    CHANGE : API ferewshing time of the docotor lay7out

--- doctor/src/pages/protected/layout.jsx
+++ doctor/src/pages/protected/layout.jsx
@@ -37,7 +37,7 @@ const Layout = ({ children }) => {
         };
 
         checkDoctorLogin(); // Run once immediately
-        const interval = setInterval(checkDoctorLogin, 15000); // every 5 min
+        const interval = setInterval(checkDoctorLogin, 1200000); // every 20 min
         return () => clearInterval(interval);
     }, []);
 
